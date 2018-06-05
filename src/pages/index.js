4404c34ec46ccefd4eaef58356bd1057dc09937a
Patrick Powell <prp1277@gmail.com>
2018-06-05 12:56:36 -0500

    graphql query names

--- src/pages/index.js
+++ src/pages/index.js
@@ -33,7 +33,7 @@ export default ({ data }) => {
 };
 
 export const query = graphql`
-  query IndexQuery {
+  query PagesQuery {
     allMarkdownRemark (sort: 
     {fields: [frontmatter___date], order: DESC})
     {
