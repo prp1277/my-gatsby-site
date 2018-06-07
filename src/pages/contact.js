40cb26d9d154cdc28a70de8c3d26b9f50d126173
Patrick Powell <prp1277@gmail.com>
2018-06-07 13:07:29 -0500

    add image and calendar post

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,18 +2,24 @@ import React from "react";
 import Link from "gatsby-link";
 
 export default () => (
-  <div style={{ alignItems: "center" }}>
+  <div>
   <h1>
     Get In Touch!
      </h1>
-      <p><a href="mailto:prp1277@gmail.com">Prp1277@gmail.com</a></p>
-   
-      <p><a href="tel:14024159083">Cell Phone</a></p>
-    
-      <p><a href="https://linkedin.com/in/prpowell1277">LinkedIn</a></p>
-    
-      <p><a href="https://github.com/prp1277">GitHub</a></p>
-
-      <p><a href="https://prp1277.github.io/resume/">Resume</a></p>
+      <p>
+        <a href="mailto:prp1277@gmail.com">Prp1277@gmail.com</a>
+        </p>
+      <p>
+        <a href="tel:14024159083">Cell Phone</a>
+      </p>
+      <p>
+        <a href="https://linkedin.com/in/prpowell1277">LinkedIn</a>
+        </p>
+      <p>
+        <a href="https://github.com/prp1277">GitHub</a>
+      </p>
+      <p>
+        <a href="https://prp1277.github.io/resume/">Resume</a>
+        </p>
   </div>
 );
--- src/pages/reminders.md
+++
@@ -1,5 +0,0 @@
----
-title: "reminders"
-permalink: "/reminders/"
----
-# Reminders:
