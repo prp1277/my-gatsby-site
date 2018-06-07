5a704034838a310bde3782540bd9bd967d23d9f1
Patrick Powell <prp1277@gmail.com>
2018-06-06 21:35:28 -0500

    update gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,31 +1,33 @@
-module.exports = 
-{
+module.exports = {
   siteMetadata: {
-    title: 'Fake News',
-    author: `Patrick Powell`,
-    description: `My Personal and Professional Blog`,
-    siteURL: `https://prp1277.github.io`,
+    title: "Fake News",
+    author: "Patrick Powell",
+    description: "My Personal and Professional Blog",
+    siteURL: "https://prp1277.github.io",
   },
   
   plugins: [
-    'gatsby-plugin-offline',
-    'gatsby-transformer-sharp',
-    'gatsby-plugin-sharp',
-  {resolve:'gatsby-transformer-remark',
-   options: {
-     plugins: ['gatsby-remark-responsive-iframe']
-    },
-  },
-  'gatsby-plugin-glamor',
-    {resolve: 'gatsby-source-filesystem', 
+    "gatsby-plugin-offline",
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-sharp",
+  {
+    resolve:"gatsby-transformer-remark",
     options: {
-      name: "prp1277.github.io",
-      path: `${__dirname}/src/`,
+     plugins: ["gatsby-remark-responsive-iframe"]
     },
   },
-    {resolve: 'gatsby-plugin-typography',
-     options: {
-       pathToConfigModule: '/src/utils/typography',
+  "gatsby-plugin-glamor",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `prp1277.github.io`,
+        path: `${__dirname}/src/`,
+    },
+  },
+    {
+      resolve: "gatsby-plugin-typography",
+      options: {
+        pathToConfigModule: `/src/utils/typography`,
       },
     },  
   ]
