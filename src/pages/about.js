f474b0f5f1238436291d4fbad34ca2d181d00245
Patrick Powell <prp1277@gmail.com>
2018-06-07 12:24:26 -0500

    abou

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,11 +5,26 @@ export default ({ data }) =>
     <h1>
     About  {data.site.siteMetadata.title}
      </h1>
+     <h3>
+       Hi, my name is Patrick Powell and if I knew how to insert a picture here, I would!
+     </h3>
      <p>
-       This is version 2.0 of my personal website using Gatsby.js and React. Things didn't go great on version 1.0 using Jekyll and Github pages, 
+       This is version 2.0 of my personal website. Things didn't go great on version 1.0 using Jekyll and Github pages, 
        so I figured I'd further complicate things and learn another language to make this one work a little bit better. 
        Maybe it will, maybe it won't. The only way we'll know is if I actually post some content and share it instead of just writing and deleting
        pages over and over again.</p>
+    <p> 
+       My plan is to learn how the "JAMStack" architecture works because that seems to be the way web development is going. 
+       It doesn't really make a lot of sense to me to learn the intimate details of a bunch of different languages all at once
+       when I can easily learn how to write and format Markdown for content and React for just about everything else. 
+       Taking the server-side components out of the mix should help to simplify things (me thinks), but I could also be
+       ass-backwards on that mentality.
+       </p>
+       <p>
+         Anyway, I'm hosting the content on Github, using Gatsby to compile everything and Netlify for continuous integration. 
+       It's all a little mmore complicated than I'd planned, but I've learned a lot along the way. There's still a lot more I have
+       to learn, but that will come in time.
+         </p>
   </div>
 
 export const query = graphql`
