d588e621ef0f43e789dbd9d5ccebc64009a82ec6
Patrick Powell <prp1277@gmail.com>
2018-07-07 23:01:47 -0500

    revert about page without image

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,13 +2,11 @@ import React from "react";
 import Wrapper from "../components/wrapper"
 import Header from "../components/header"
 import Footer from "../components/footer"
-import linkedin from "../img/linkedin.jpg"
 
 export default ({ data }) => 
   <Wrapper>
     <Header />
     <div className="Content" css={{ marginLeft:`auto`, marginRight: `auto` , maxWidth: `42rem` }}>
-    <img src={ linkedin } css={{ float: `center` }} alt=""  />
     <h1 css={{ textAlign: `center` }}
     >About  {data.site.siteMetadata.author}
     </h1>
@@ -50,3 +48,15 @@ export const query = graphql`
       }
   }
 `
+
+/*
+import Footer from "../components/footer"
+
+export default ({ data }) => 
+  <Wrapper>
+    <Header />
+    <div className="Content" css={{ marginLeft:`auto`, marginRight: `auto` , maxWidth: `42rem` }}>
+    <h1 css={{ textAlign: `center` }}
+    >About  {data.site.siteMetadata.author}
+
+*/
