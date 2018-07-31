54e8249be4f4e4f66ca5f946180caec0ac8227a7
Patrick Powell <prp1277@gmail.com>
2018-07-31 01:51:00 -0500

    empasis in about.js

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -19,37 +19,44 @@ export default ({ data }) => {
           />
         </center>
         <p>
-          I'm originally from Omaha, NE, went to Missouri State University in
+          I'm originally
+          <b>from Omaha, NE, went to Missouri State University</b> in
           Springfield, MO and moved to Kansas City after graduating in December
-          2015 with a B.S. in Finance and a Minor in Accounting.
+          2015 with a <b>B.S. in Finance and a Minor in Accounting.</b>
         </p>
         <p>
-          My background since graduating has primarily been focused on business
-          operations, first in the call center at Waddell & Reed, then in Mutual
-          Fund Operations at State Street and most recently in the FoodService
+          <b>
+            My background since graduating has primarily been focused on
+            business operations
+          </b>, first in the call center at Waddell & Reed, then in Mutual Fund
+          Operations at State Street and most recently in the FoodService
           Industry with Reinhart FoodService.
         </p>
         <p>
-          During my time with State Street, I identified the dependence on
-          Microsoft Excel and decided to learn everything that I could about the
-          software. Using Microsoft OneNote, I created documentation for
-          Formulas, Keyboard Shortcuts and general notes to help improve my
-          skills. This did not go unnoticed, and a few months later I applied
-          and accepted a role as the Purchasing Assistant at Reinhart
-          Foodservice.
+          <b>
+            During my time with State Street, I identified the dependence on
+            Microsoft Excel and decided to learn everything that I could about
+            the software.
+          </b>{" "}
+          Using Microsoft OneNote, I created documentation for Formulas,
+          Keyboard Shortcuts and general notes to help improve my skills. This
+          did not go unnoticed, and a few months later I applied and accepted a
+          role as the Purchasing Assistant at Reinhart Foodservice.
         </p>
         <p>
           At Reinhart, I continued to advance my Excel skills. I starting with
-          VBA Macros to automate daily and weekly tasks. When I learned about
-          Power Queries I started mixing and matching between the two, using VBA
-          primarily for formatting and Power Queries for analysis.
+          <b> VBA Macros to automate daily and weekly tasks.</b> When I learned
+          about Power Queries I started mixing and matching between the two,
+          using VBA primarily for formatting and Power Queries for analysis.
         </p>
         <p>
           After diving deeper into Power Query I stumbled upon the free, desktop
           version of Power BI. Since it used the familiar Power Pivot features I
-          had previously learned, the learning curve came naturally and I
-          immediately preferred Power Bi's ETL model for cleaning data and
-          creating visualizations.
+          had previously learned, the learning curve came naturally and{" "}
+          <b>
+            I immediately preferred Power Bi's ETL model for cleaning data and
+            creating visualizations.
+          </b>
         </p>
         <p>
           After parting ways with Reinhart in late February I realized that the
@@ -58,9 +65,11 @@ export default ({ data }) => {
           literally get data from anywhere.
         </p>
         <p>
-          Since then, I've built two websites (you're looking at version 2),
-          familiarized myself with JS, HTML and CSS and learned a few hard
-          lessons about Data Science.
+          <b>
+            Since then, I've built two websites (you're looking at version 2),
+            familiarized myself with JS, HTML and CSS and learned a few hard
+            lessons about Data Science.
+          </b>
         </p>
         <p>
           Throughout my experiences I've found that observing a process from
